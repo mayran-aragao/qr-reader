ce392d6a5213765a31889c7620aa9b485ff5b889
Mayran Aragao <mayran_aragao@hotmail.com>
2022-09-05 13:11:08 -0300

    refactor: realm connection

--- src/databases/realm.ts
+++ src/databases/realm.ts
@@ -1,9 +1,11 @@
-import Realm, { schemaVersion } from "realm";
+import Realm from "realm";
 import { ProductSchema } from "./schemas/ProductSchema";
 
-export const getRealm = async () =>
-  await Realm.open({
-    path: "qr-reader",
-    schema:[ProductSchema],
-    schemaVersion:3,
-  });
+const realm = Realm.open({
+  path: "qr-reader",
+  schema: [ProductSchema],
+  schemaVersion: 3,
+});
+
+export const getRealm = () => realm
+  
