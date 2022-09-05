af44400b020e316a0e58a6b620a9f86226be899f
Mayran Aragao <mayran_aragao@hotmail.com>
2022-09-05 13:10:05 -0300

    feat: added loading component

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import { ActivityIndicator } from "react-native";
-import {
-  Container,
-} from "./styled";
+import { Container, Text } from "./styled";
+
+import * as Animatable from "react-native-animatable";
 
 const Loading = () => {
-  
   return (
     <Container>
-      <ActivityIndicator size='small' color='#fe563f'/>
+      <ActivityIndicator size="large" color="#fe563f" />
+      <Animatable.View animation="pulse" easing="ease-out" iterationCount="infinite">
+        <Text>Enviando...</Text>
+      </Animatable.View>
     </Container>
   );
 };
--- src/components/Loading/styled.tsx
+++ src/components/Loading/styled.tsx
@@ -1,7 +1,14 @@
 import styled from "styled-components/native";
 
+
 export const Container = styled.SafeAreaView`
   flex: 1;
   justify-content: center;
   align-items: center;
+`;
+export const Text = styled.Text`
+  font-size: 20px;
+  color: #fe563f;
+  margin-top: 10px;
+  font-weight: 500;
 `;
